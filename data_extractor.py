236024f67a85a6a3719150ea66b3b3161ab4fbbc
ojoadekunle <ojoadekunle84@gmail.com>
2022-10-16 22:15:14 +0100

    Excel- Wrangling

--- data_extractor.py
+++ data_extractor.py
@@ -9,53 +9,55 @@ import openpyxl
 import os
 
 # File name.
-cont_folder = 'C:/Users/Services/OneDrive - IL Bagno Bathrooms + Design/Documents/Python Scripts/container_file'
+cont_folder = 'C:/Users/User1/Documents/Data_Project/excel_data-wrangler/excel_files/container_file'
 
 # Print list of file in a folder. 
 files = os.listdir(cont_folder)
 
 # Container code 
-code = []
+codes = []
 
 for file in files:
     extr = file.split(' ')[1].split('.')[0]
-    code.append(extr)
+    codes.append(extr)
 
 # print(code)
 
 # Creating extract file. 
-saving_path = 'C:/Users/Services/OneDrive - IL Bagno Bathrooms + Design/Documents/Python Scripts/extract.xlsx'
+saving_path = 'C:/Users/User1/Documents/Data_Project/excel_data-wrangler/extract.xlsx'
 
 
 # Creating excel sheet. 
 wb = openpyxl.workbook.Workbook()
 
-for i in range(len(code)):
-    i = wb.create_sheet(code[i])
+for i in range(len(codes)):
+    i = wb.create_sheet(codes[i])
     
 
 # loading copy file
 print('Loading coping sheet.', '\n')
-copy_path = 'C:/Users/Services/OneDrive - IL Bagno Bathrooms + Design/Documents/Python Scripts/container_file/CONTAINER 196.xlsx'
+for code in codes: 
+    copy_path = f'C:/Users/User1/Documents/Data_Project/excel_data-wrangler/excel_files/container_file/CONTAINER {code}.xlsx'
 
-copy_wb = openpyxl.load_workbook(copy_path)
-copy_ws = copy_wb[code[0]]
+    copy_wb = openpyxl.load_workbook(copy_path)
+    copy_ws = copy_wb[code]
 
-past_ws = wb[code[0]]
+    past_ws = wb[code]
 
-# Get maximum row and columns for colume
-print('Getting Maximum row & column from data.', '\n')
-rm = copy_ws.max_row
-cm = copy_ws.max_column
+    # Get maximum row and columns for colume
+    print(f'Getting Maximum row & column from data. Container-{code}', '\n')
+    rm = copy_ws.max_row
+    cm = copy_ws.max_column
 
-# coping sheet to extract folder. 
-print('Coping data from copy sheet', '\n')
-for i in range(1, rm + 1): 
-    for j in range(1, cm + 1):
-        c = copy_ws.cell(row = i , column = j)
-        
-        past_ws.cell(row = i, column = j).value = c.value
-print('Coping sheet complete', '\n')
+    # coping sheet to extract folder. 
+    print(f'Coping data from copy sheet. Container-{code}', '\n')
+    for i in range(1, rm + 1): 
+        for j in range(1, cm + 1):
+            c = copy_ws.cell(row = i , column = j)
+            
+            past_ws.cell(row = i, column = j).value = c.value
+    print(f'Coping sheet complete. Container-{code}', '\n\n')
+
 
 wb.save(saving_path)
 print('Saving Pasting sheet', '\n')
