5ebec13f32788700720d16009b52db7c6eaed103
ojoadekunle <ojoadekunle84@gmail.com>
2022-11-06 17:17:18 +0100

    Excel Data-wrangling

--- excel_wrangler.py
+++ excel_wrangler.py
@@ -12,5 +12,19 @@ import os
 from openpyxl import Workbook as wb 
 from openpyxl import load_workbook as lwb
 
+# File name.
+cont_folder = 'C:/Users/User1/Documents/Data_Project/excel_data-wrangler/excel_files/container_file'
+
+# Print list of file in a folder. 
+files = os.listdir(cont_folder)
+
+# Container code 
+codes = []
+
+for file in files:
+    extr = file.split(' ')[1].split('.')[0]
+    codes.append(extr)
+
+print(codes)
 # loaing packages. 
-xl = pd.read_excel('extract_2022-11.xlsx', sheet_name = '367-369',engine='openpyxl', na_values=['nan'])
+#xl = pd.read_excel('extract_2022-11.xlsx', sheet_name = '367-369',engine='openpyxl', na_values=['nan'])
