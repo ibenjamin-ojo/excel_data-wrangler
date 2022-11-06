efa23f80aee3ab082cf48ceb085a5e3af2c32854
ojoadekunle <ojoadekunle84@gmail.com>
2022-11-06 13:51:22 +0100

    Excel Data-wrangling

--- excel_wrangler.py
+++ excel_wrangler.py
@@ -4,3 +4,13 @@ Created on Mon Oct 31 14:31:59 2022
 
 @author: Benjamin Ojo
 """
+# Import packages. 
+import pandas as pd 
+import os 
+
+# Excel packages.
+from openpyxl import Workbook as wb 
+from openpyxl import load_workbook as lwb
+
+# loaing packages. 
+xl = pd.read_excel('extract_2022-11.xlsx', sheet_name = '367-369',engine='openpyxl', na_values=['nan'])
