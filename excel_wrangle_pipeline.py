1bde84bd37004643f2192c82523ed405a3a88163
ojoadekunle <ojoadekunle84@gmail.com>
2022-11-24 23:06:23 +0100

    Excel Wrangle Pipeline

--- excel_wrangle_pipeline.py
+++ excel_wrangle_pipeline.py
@@ -4,4 +4,11 @@ Created on Thu Nov 24 22:55:44 2022
 
 @author: User1
 """
+import pandas as pd
+import os
 
+'''
+This python combines the pervious script (excel_extractor, excel_wrangler, 
+and excel_marger) into one unifered pipeline that extract, clean and formate
+the data, and 
+'''
