e2c75dcf439de65829b16eabda835dadd2cfb5b2
ojoadekunle <ojoadekunle84@gmail.com>
2023-01-05 19:06:22 +0100

    Excel Pipeline

--- excel_pipeline.py
+++ excel_pipeline.py
@@ -18,4 +18,13 @@ the data, and merge excel sheet into one workbook.
 # Creating a data wrangling pipeline. 
 
 Class excel_pipeline:
-    """  """
+    """ 
+    This combine the last three python script into one that can 
+    performe the three functions.
+    """
+    def __init__(self, file_path, save_path): 
+        
+        self.file_path = file_path
+        self.save_path = save_path
+        
+    def file_code(self, )
