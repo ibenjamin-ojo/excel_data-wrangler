fcee840aa2881a3a8f97e360f79145db9bc5ee88
ojoadekunle <ojoadekunle84@gmail.com>
2023-01-06 23:29:23 +0100

    Excel Pipeline

--- excel_pipeline.py
+++ excel_pipeline.py
@@ -185,6 +185,47 @@ Class Excel_Pipeline:
         """
         csv_path = self.save_path + f"/csv_files"
         
+        # Print list of file in a folder. 
+        files = os.listdir(cont_folder)
+        print(files)
+
+        excel_append = pd.DataFrame()
+        for file in files[:50]:
+            origi = pd.read_csv(f'csv_files/{file}')
+            excel_append = excel_append.append(origi, ignore_index =  True)
+
+        excel_append = excel_append.iloc[:, :12]
+
+        excel_append1 = pd.DataFrame()
+        for file in files[50:100]:
+            origi = pd.read_csv(f'csv_files/{file}')
+            excel_append1 = excel_append1.append(origi, ignore_index =  True)
+
+        excel_append1 = excel_append1.iloc[:, :12]
+
+        excel_append2 = pd.DataFrame()
+        for file in files[100:150]:        
+            origi = pd.read_csv(f'csv_files/{file}')
+            excel_append2 = excel_append2.append(origi, ignore_index = True)
+
+        excel_append2 = excel_append2.iloc[:, :12]
+
+        excel_append3 = pd.DataFrame()
+        for file in files[150:240]:
+            origi = pd.read_csv(f'csv_files/{file}')
+            excel_append3 = excel_append3.append(origi, ignore_index = True)
+
+        excel_append3 = excel_append3.iloc[:, :12]
+
+        excel_merge = excel_append.append(excel_append1, ignore_index = True)
+
+        excel_merge.append(excel_append2, ignore_index = True)
+
+        excel_merge.append(excel_append3, ignore_index = True)
+
+        excel_merge
+
+        excel_merge.to_excel('excel_merge.xlsx')
         
             
             
